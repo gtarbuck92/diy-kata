c0d0c12865a31dfebc99838959dec414cbdfa87b
George Tarbuck <gtarbuck2692@googlemail.com>
2020-03-11 17:21:01 +0000

    Complete

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,9 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+   if (namesObj.length === 0) return '';
+   if (namesObj.length === 1) return namesObj[0].name; 
+   let result = namesObj.map(person => person.name);
+   let lastName = result.pop();
+   return result.join(', ') + ' & ' + lastName
+};
 
 module.exports = joinNames;
